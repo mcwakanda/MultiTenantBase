34a4cc49fba02a484db1d22bd88c71c9918a9eea
Greg McCarvell <greg@gregmccarvell.com>
2014-01-12 23:00:56 -0500

    Started on login and account creation

--- MultiTenantBase/Model.js
+++ MultiTenantBase/Model.js
@@ -1,1 +1,1 @@
-include("./Model/User/User-methods.js");include("./Model/User/User-events.js");include("./Model/DataItem/DataItem-events.js");
+include("./Model/User/User-methods.js");include("./Model/User/User-events.js");include("./Model/DataItem/DataItem-events.js");include("./Model/Account/Account-methods.js");
---
+++ MultiTenantBase/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,32 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+	var loginViewButton = {};	// @button
+	var registerViewButton = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		$$('registerComp').loadComponent('/components/register.waComponent');
+	};// @lock
+
+	loginViewButton.click = function loginViewButton_click (event)// @startlock
+	{// @endlock
+		$$('registerComp').loadComponent('/components/login.waComponent');
+	};// @lock
+
+	registerViewButton.click = function registerViewButton_click (event)// @startlock
+	{// @endlock
+		$$('registerComp').loadComponent('/components/register.waComponent');
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("loginViewButton", "click", loginViewButton.click, "WAF");
+	WAF.addListener("registerViewButton", "click", registerViewButton.click, "WAF");
+// @endregion
+};// @endlock
