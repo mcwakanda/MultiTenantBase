bc71784b781417b4e923b73867be9625cf962308
Greg McCarvell <greg@gregmccarvell.com>
2014-01-14 19:48:36 -0500

    added login structure

--- MultiTenantBase/WebFolder/components/register.waComponent/register.js
+++ MultiTenantBase/WebFolder/components/register.waComponent/register.js
@@ -11,13 +11,49 @@ function constructor (id) {
 	// @endregion// @endlock
 
 	this.load = function (data) {// @lock
-
+		var appLogin = function(username, password){
+			WAF.directory.loginByPassword(username, password,{
+				onSuccess: function(event){
+					
+				},
+				onError: {
+					
+				}
+			});
+		}
 	// @region namespaceDeclaration// @startlock
+	var registerButton = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	registerButton.click = function registerButton_click (event)// @startlock
+	{// @endlock
+		var accountName = $$(getHtmlId('accountNameInput')).getValue();
+		var username = $$(getHtmlId('usernameInput')).getValue();
+		var password = $$(getHtmlId('passwordInput')).getValue();
+		
+		ds.Account.callMethod({
+			method: 'registerAccount',
+			arguments: [{
+				name: accountName,
+				username: username,
+				password: password
+			}],
+			onSuccess: function(event){
+				if(event.result){
+					appLogin(username, password);
+				} else
+			},
+			onError: function(err){
+				//handle error
+				alert('Could not create new account');
+			}
+		});
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_registerButton", "click", registerButton.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
