c5658522a625f07524db5cc5a741eaa6a9350c62
Greg McCarvell <greg@gregmccarvell.com>
2014-01-30 06:35:36 -0500

    added restricting query to user - fixed general mechanism

--- MultiTenantBase/WebFolder/app.waPage/scripts/index.js
+++ MultiTenantBase/WebFolder/app.waPage/scripts/index.js
@@ -2,17 +2,29 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var saveDataItemButton = {};	// @button
+	var logoutButton = {};	// @button
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	saveDataItemButton.click = function saveDataItemButton_click (event)// @startlock
+	{// @endlock
+		sources.dataItem.save();
+	};// @lock
+
+	logoutButton.click = function logoutButton_click (event)// @startlock
+	{// @endlock
+		WAF.directory.logout();
+		window.location = "/";
+	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
 		if(WAF.directory.currentUser() != null){
 			ds.User.getSettings({
 				onSuccess: function(event){
-					debugger;
 					APP.userSettings = event.result;
 				}
 			});
@@ -25,6 +37,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("saveDataItemButton", "click", saveDataItemButton.click, "WAF");
+	WAF.addListener("logoutButton", "click", logoutButton.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
 };// @endlock
